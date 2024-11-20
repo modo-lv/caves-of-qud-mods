f751e7b8a1353059c59b824ab3b59816d6acab58
modo_lv <modo@modo.lv>
2024-11-20 02:50:00 +0200

    Start/Restart QuD on run

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
+taskkill /f /im CoQ.exe 
 robocopy . "c:\users\martin\AppData\LocalLow\Freehold Games\CavesOfQud\Mods\SkillTraining" *.cs /s /purge
+start "" "c:\games\steam\steamapps\common\Caves of Qud\CoQ.exe"
