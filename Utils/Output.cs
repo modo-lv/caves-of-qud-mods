64d9769dc6e200e4a01e95f5f01287b2d6244da6
modo_lv <modo@modo.lv>
2024-11-20 02:49:44 +0200

    Basic logging.

---
+++ Utils/Output.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+using XRL.Messages;
+using XRL.UI;
+
+namespace SkillTraining.Utils {
+  public static class Output {
+    /// <summary>
+    /// Log a message to the game's log file. 
+    /// </summary>
+    public static void Log(String text) {
+      var dt = DateTime.Now.ToString(@"yyyy-MM-dd HH:mm:ss.fffzzz", CultureInfo.InvariantCulture);
+      Debug.Log($"[SkillTraining][{dt}] {text}");
+      // For now, output everything in game as well for easier access.
+      Message(text);
+    }
+
+    /// <summary>
+    /// Add a message to the in-game message log.
+    /// </summary>
+    public static void Message(String text) {
+      MessageQueue.AddPlayerMessage(text);
+    }
+
+    /// <summary>
+    /// Show a popup message to the player in game.
+    /// </summary>
+    public static void Alert(String text) {
+      Popup.Show(text);
+    }
+  }
+}
