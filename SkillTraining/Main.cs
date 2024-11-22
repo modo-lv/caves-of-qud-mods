87c0357c0710589196c8ec755ea6d6a127a32da3
modo_lv <modo-lv@users.noreply.github.com>
2024-11-22 20:06:13 +0200

    Rename `Mod` to `Main`.

--- SkillTraining/Main.cs
+++ SkillTraining/Main.cs
@@ -4,14 +4,17 @@ using XRL;
 using XRL.World;
 
 namespace SkillTraining {
-  [HasCallAfterGameLoaded][PlayerMutator]
-  public class Mod : IPlayerMutator {
+  /// <summary>Main "entry point" for the mod functionality.</summary>
+  /// <remarks>Attaches the <see cref="PointTracker"/> part to the player object.</remarks>
+  [HasCallAfterGameLoaded] [PlayerMutator]
+  public class Main : IPlayerMutator {
     [CallAfterGameLoaded]
     public static void OnGameLoaded() {
-      Output.DebugLog($"Game loaded, ensuring that [{nameof(PointTracker)}] part is attached to [{Req.Player}]...");
+      Output.DebugLog(
+        $"Game loaded, ensuring that [{nameof(PointTracker)}] part is attached to [{Req.Player}]...");
       Req.Player.RequirePart<PointTracker>();
     }
-    
+
     public void mutate(GameObject player) {
       Output.DebugLog($"New game started, attaching [{nameof(PointTracker)}] part to [{player}]...");
       player.RequirePart<PointTracker>();
