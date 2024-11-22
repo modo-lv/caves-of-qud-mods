754f78b06aa9deb49a283536d406e4e145f0e231
modo_lv <modo-lv@users.noreply.github.com>
2024-11-22 18:42:08 +0200

    Reorganize into a multi-project solution.

--- SkillTraining/run.bat
+++ SkillTraining/run.bat
@@ -1,3 +1,4 @@
+cd %~dp0
 taskkill /f /im CoQ.exe 
 robocopy . "c:\users\martin\AppData\LocalLow\Freehold Games\CavesOfQud\Mods\SkillTraining" *.cs /s /purge
 start "" "c:\games\steam\steamapps\common\Caves of Qud\CoQ.exe"
