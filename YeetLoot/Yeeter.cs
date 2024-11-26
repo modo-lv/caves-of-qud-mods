75c8027e02779ad4f3bc061ea261297000118ac1
modo_lv <modo-lv@users.noreply.github.com>
2024-11-26 02:08:43 +0200

    [YeetLoot] Use an intermediary chest.

--- YeetLoot/Yeeter.cs
+++ YeetLoot/Yeeter.cs
@@ -1,38 +1,71 @@
 ﻿using System;
 using System.Linq;
-using HarmonyLib;
 using ModoMods.Core.Utils;
+using ModoMods.LootYeet.Data;
 using Newtonsoft.Json;
-using UnityEngine;
 using Wintellect.PowerCollections;
 using XRL;
 using XRL.UI;
-using XRL.Wish;
 using XRL.World;
+using XRL.World.Parts;
+using EventNames = ModoMods.Core.Data.EventNames;
 using GameObject = XRL.World.GameObject;
 
 namespace ModoMods.LootYeet {
-  [HarmonyPatch][HasWishCommand]
+  [Serializable]
   public class Yeeter : ModPart {
-    [SerializeField] public GameObject? Chest;
-    public override Set<Int32> WantEventIds => new Set<Int32> { InventoryActionEvent.ID };
+    public GameObject? Chest;
+
+    public override Set<Int32> WantEventIds => new Set<Int32> {
+      CommandEvent.ID,
+      GenericCommandEvent.ID,
+      CommandTakeActionEvent.ID,
+      InventoryActionEvent.ID,
+    };
 
     public override Boolean HandleEvent(InventoryActionEvent ev) {
-      Output.DebugLog(ev.Actor);
-      Output.DebugLog(ev.Command);
-      Output.DebugLog(ev.Item.Blueprint);
+      if (!ev.Actor.IsPlayer()
+          || ev.Command != EventNames.CommandDropObject
+          || ev.Item.Blueprint != BlueprintNames.YeetLootChest)
+        return base.HandleEvent(ev);
 
-      if (ev.Actor.IsPlayer()
-          && ev.Command == "CommandDropObject"
-          && ev.Item.Blueprint == "ModoMods_LootYeet_Chest") {
+      this.Chest = ev.Item;
+      Output.DebugLog($"[{ev.Actor}] dropped [{ev.Item.Blueprint}], loot yeeting now possible.");
 
-        Output.DebugLog($"[{ev.Actor}] dropped [{ev.Item.Blueprint}]");
-
-        this.Chest = ev.Item;
-      }
       return base.HandleEvent(ev);
     }
 
+    public override Boolean HandleEvent(CommandEvent ev) {
+      if (ev.Command != YlEventNames.YeetLoot)
+        return base.HandleEvent(ev);
+      if (this.Chest == null) {
+        Popup.PickOption(
+          Title: "No loot chest",
+          Intro: "Items can only be yeeted to a loot chest that's been placed somewhere.\n\n" +
+                 "If you don't have the chest in your inventory either, wish for " +
+                 "{{Y|" + BlueprintNames.YeetLootChest + "}} to correct this.",
+          AllowEscape: true
+        );
+        return base.HandleEvent(ev);
+      }
+      if (this.Chest.IsInvalid()) {
+        Popup.PickOption(
+          Title: "Loot chest invalid",
+          Intro: "Something has gone wrong with the chest placed in the world.\n\n" +
+                 "Wish for {{Y|" + BlueprintNames.YeetLootChest + "}} to get a new one and use that.",
+          AllowEscape: true
+        );
+        return base.HandleEvent(ev);
+      }
 
+      var temp = GameObject.CreateUnmodified("Chest");
+      temp.DisplayName = "extradimensional yeeter";
+      TradeUI.ShowTradeScreen(temp, 0.0f, TradeUI.TradeScreenMode.Container);
+      while (!temp.Inventory.Objects.IsNullOrEmpty()) {
+        this.Chest.Inventory.AddObject(temp.Inventory.GetFirstObject());
+      }
+      
+      return base.HandleEvent(ev);
+    }
   }
 }
