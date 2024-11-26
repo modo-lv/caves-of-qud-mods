52fe8fef615bae5ff88b400909071de568caeafe
modo_lv <modo-lv@users.noreply.github.com>
2024-11-26 23:55:46 +0200

    [ItemRecoiler] Changed to zone-based functionality.

--- ItemRecoiler/Main.cs
+++ ItemRecoiler/Main.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using ModoMods.Core.Utils;
 using ModoMods.ItemRecoiler.Data;
+using Newtonsoft.Json;
 using XRL;
+using XRL.UI;
+using XRL.Wish;
 using XRL.World;
 
 namespace ModoMods.ItemRecoiler {
-  [HasCallAfterGameLoaded][PlayerMutator]
+  [HasCallAfterGameLoaded][PlayerMutator][HasWishCommand]
   public class Main : IPlayerMutator {
     public static GameObject Player =>
       The.Player ?? throw new NullReferenceException("[The.Player] is null.");
 
     public static void Init(GameObject player) {
       if (!player.HasPart<Recoiler>()
-          || player.GetPart<Recoiler>()?.Storage == null
+          || player.GetPart<Recoiler>()?.Escrow == null
           && !player.Inventory.HasObject(IrBlueprintNames.Storage)) {
         Output.DebugLog($"[{player}] does not appear to own a [{IrBlueprintNames.Storage}], placing in inventory...");
         player.Inventory.AddObject(IrBlueprintNames.Storage);
@@ -25,5 +28,11 @@ namespace ModoMods.ItemRecoiler {
 
     /// <summary>Load game.</summary>
     [CallAfterGameLoaded] public static void OnGameLoaded() { Init(Player); }
+
+
+    [WishCommand("q")]
+    public static void Open() {
+      TradeUI.ShowTradeScreen(Player.RequirePart<Recoiler>()!.Escrow, 0.0f, TradeUI.TradeScreenMode.Container);
+    }
   }
 }
--- ItemRecoiler/Recoiler.cs
+++ ItemRecoiler/Recoiler.cs
@@ -8,48 +8,72 @@ using EventNames = ModoMods.Core.Data.EventNames;
 using GameObject = XRL.World.GameObject;
 
 namespace ModoMods.ItemRecoiler {
+  /// <summary>Provides item recoil functionality.</summary>
+  /// <remarks>
+  /// Actually sending items to distant zones isn't possible as game objects get serialized when the player
+  /// moves too far away. Instead, we use an abstract chest object attached to the player that the player
+  /// has no direct access to.
+  /// 
+  /// When the player drops the recoil receiver, the zone ID of the location is recorded and item recoiling
+  /// enabled. "Recoiled" items are stored in <see cref="Escrow"/>, and, once the player returns to the zone,
+  /// automatically transferred to the receiver chest at the end of the turn.
+  /// </remarks>
   [Serializable]
   public class Recoiler : ModPart {
-    public GameObject? Storage;
+    private GameObject? _storage;
+    public GameObject Escrow => this._storage ??= GameObject.CreateUnmodified("Chest");
+
+    public String? ZoneId;
 
     public override Set<Int32> WantEventIds => new Set<Int32> {
       CommandEvent.ID,
       GenericCommandEvent.ID,
       CommandTakeActionEvent.ID,
-      InventoryActionEvent.ID,
+      EndTurnEvent.ID
     };
 
-    public override Boolean HandleEvent(InventoryActionEvent ev) {
-      if (!ev.Actor.IsPlayer()
-          || ev.Command != EventNames.CommandDropObject
-          || ev.Item.Blueprint != IrBlueprintNames.Storage)
-        return base.HandleEvent(ev);
+    public override Boolean HandleEvent(EndTurnEvent ev) {
+      if (this.ZoneId == null
+          && Main.Player.CurrentZone.HasObject(IrBlueprintNames.Storage)) {
+        this.ZoneId = Main.Player.CurrentZone.ZoneID;
+        Output.DebugLog($"Found receiver chest in zone [{this.ZoneId}], recoiling enabled.");
+      }
 
-      this.Storage = ev.Item;
-      Output.DebugLog($"[{ev.Actor}] placed [{ev.Item.Blueprint}], recoiled items should go to it.");
-
+      if (Main.Player.CurrentZone.ZoneID == this.ZoneId
+          && !Main.Player.CurrentZone.HasObject(IrBlueprintNames.Storage)) {
+        this.ZoneId = null;
+        Output.DebugLog($"Recoil receiver in [{Main.Player.CurrentZone.ZoneID}] removed, recoiling disabled.");
+      }
+      
+      if (Main.Player.CurrentZone.ZoneID == this.ZoneId
+          && !this.Escrow.Inventory.Objects.IsNullOrEmpty()) {
+        var chest = Main.Player.CurrentZone.FindObject(IrBlueprintNames.Storage);
+        var container = Main.Player.Inventory;
+        if (chest == null) {
+          Output.Alert(
+            "Recoiled item receiver seems to have disappeared. " +
+            "Placing recoiled items back in the inventory."
+          );
+        } else {
+          container = chest.Inventory;
+        }
+        while (!this.Escrow.Inventory.Objects.IsNullOrEmpty()) {
+          var item = this.Escrow.Inventory.GetFirstObject();
+          container.AddObject(item);
+        }
+      }
       return base.HandleEvent(ev);
     }
-
+    
     public override Boolean HandleEvent(CommandEvent ev) {
       if (ev.Command != IrEventNames.TransmitCommand)
         return base.HandleEvent(ev);
-      if (this.Storage == null) {
+      if (this.ZoneId == null) {
         Popup.PickOption(
-          Title: "No recoil chest",
+          Title: "No recoil receiver",
           Intro: "Items can only be recoiled to a manually placed recoil storage chest.\n\n" +
                  "If you don't have one in your inventory either, wish for " +
                  "{{Y|" + IrBlueprintNames.Transmitter + "}} (and don't forget to pick it up!).",
-          AllowEscape: true,
-          Options: Array.Empty<String>()
-        );
-        return base.HandleEvent(ev);
-      }
-      if (this.Storage.IsInvalid()) {
-        Popup.PickOption(
-          Title: "Recoil chest invalid",
-          Intro: "Something has gone wrong with the recoil chest placed in the world.\n\n" +
-                 "Wish for {{Y|" + IrBlueprintNames.Transmitter + "}} to get a new one and place that.",
           AllowEscape: true,
           Options: Array.Empty<String>()
         );
@@ -61,10 +85,10 @@ namespace ModoMods.ItemRecoiler {
       var total = 0;
       while (!transmitter.Inventory.Objects.IsNullOrEmpty()) {
         var item = transmitter.Inventory.GetFirstObject();
-        this.Storage.Inventory.AddObject(item);
+        this.Escrow.Inventory.AddObject(item);
         total += item.Count;
       }
-      Output.Message(total + " item(s) sent to " + this.Storage.DisplayName + ".");
+      Output.Message(total + " item(s) recoiled.");
 
       return base.HandleEvent(ev);
     }
