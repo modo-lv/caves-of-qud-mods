79b9d783cc82f0db9a96b2493bc7c000dd29e746
modo_lv <modo-lv@users.noreply.github.com>
2024-11-28 01:26:12 +0200

    [LootRecoil] A few name and description updates.

--- LootRecoil/Data/LrBlueprintNames.cs
+++ LootRecoil/Data/LrBlueprintNames.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using XRL.World.Parts;
 
 namespace ModoMods.LootRecoil.Data {
   public abstract class LrBlueprintNames {
-    public const String Receiver = "ModoMods:LootRecoil:Receiver";
-    public const String Transmitter = "ModoMods:LootRecoil:Transmitter";
+    /// <summary>
+    /// The modified <see cref="ProgrammableRecoiler"/> that players activate to recoil items.
+    /// </summary>
+    public const String Recoiler = "ModoMods:LootRecoil:Recoiler";
     
-    public const String Recoiler = "ModoMods:LootRecoil:LootRecoiler";
+    /// <summary>The chest where recoiled items are sent to.</summary>
+    public const String Receiver = "ModoMods:LootRecoil:Receiver";
+    
+    /// <summary>The temporary chest that does the actual teleporting.</summary>
+    public const String Transmitter = "ModoMods:LootRecoil:Transmitter";
   }
 }
