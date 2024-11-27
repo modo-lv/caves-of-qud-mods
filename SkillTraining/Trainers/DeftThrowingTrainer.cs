eceab542a8af44ccd8f85d5bcc03d62282ee2bb0
modo_lv <modo-lv@users.noreply.github.com>
2024-11-27 02:26:16 +0200

    [SkillTraining] Fix (hopefully) thrown weapons causing an NRE.

--- SkillTraining/Trainers/DeftThrowingTrainer.cs
+++ SkillTraining/Trainers/DeftThrowingTrainer.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using ModoMods.Core.Data;
 using ModoMods.Core.Utils;
 using ModoMods.SkillTraining.Data;
-using ModoMods.SkillTraining.Utils;
-using Wintellect.PowerCollections;
 using XRL;
 using XRL.World;
 
@@ -13,50 +11,60 @@ namespace ModoMods.SkillTraining.Trainers {
   /// <remarks>
   /// Attached to the player to watch for equipment changes.
   /// When a throwing weapon is equipped, attaches to that item to listen for the throwing event.
-  /// When the weapon is thrown, the tracker attaches itself to the intended target,
-  /// to listen for the damage taken event.
+  /// When the weapon is thrown, attaches itself to the intended target, to listen for the "hit landed" event.
   /// </remarks>
   public class DeftThrowingTrainer : ModPart {
-    public GameObject? Weapon;
-
     public override ISet<Int32> WantEventIds => new HashSet<Int32> {
       EquipperEquippedEvent.ID,
       DefenderMissileHitEvent.ID,
+      AfterThrownEvent.ID,
     };
-
-    public override Boolean HandleEvent(EquipperEquippedEvent ev) {
-      if (ev.Item.IsEquippedAsThrownWeapon())
-        ev.Item.RequirePart<DeftThrowingTrainer>();
-      return base.HandleEvent(ev);
-    }
-
     public override void Register(GameObject obj, IEventRegistrar reg) {
       obj.RegisterPartEvent(this, EventNames.BeforeThrown);
       base.Register(obj, reg);
     }
 
+    /// <summary>Attaches to the equipped item.</summary>
+    public override Boolean HandleEvent(EquipperEquippedEvent ev) {
+      if (ev.Actor?.IsPlayer() == true && ev.Item.IsEquippedAsThrownWeapon())
+        ev.Item.RequirePart<DeftThrowingTrainer>();
+      return base.HandleEvent(ev);
+    }
+    
+    /// <summary>Attaches to the intended target.</summary>
+    public override Boolean FireEvent(Event ev) {
+      if (ev.ID != EventNames.BeforeThrown)
+        return base.FireEvent(ev);
+
+      // Attach this tracker to the target creature, to detect when it gets hit.
+      var target = ev.GetGameObjectParameter("ApparentTarget");
+      if (target?.IsCreature == true)
+        target.RequirePart<DeftThrowingTrainer>();
+
+      return base.FireEvent(ev);
+    }
+    
+    /// <summary>Processes a successful hit.</summary>
     public override Boolean HandleEvent(DefenderMissileHitEvent ev) {
       if (ev.Launcher != null
           || ev.Attacker?.IsPlayer() != true
           || ev.Defender?.IsCreature != true)
         return base.HandleEvent(ev);
 
-      Output.DebugLog($"[{ev.Defender}] hit with [{this.Weapon}].");
+      Output.DebugLog($"[{ev.Defender}] hit with with a thrown weapon.");
       Main.PointTracker.HandleTrainingAction(PlayerAction.ThrownWeaponHit);
 
       return base.HandleEvent(ev);
     }
 
-    public override Boolean FireEvent(Event ev) {
-      if (ev.ID != EventNames.BeforeThrown)
-        return base.FireEvent(ev);
-
-      // Attach this tracker to the target creature, to detect when it gets hit.
-      var target = ev.GetParameter("ApparentTarget") as GameObject;
-      if (target?.IsCreature == true)
-        target.RequirePart<DeftThrowingTrainer>().Weapon = this.ParentObject;
-
-      return base.FireEvent(ev);
+    /// <summary>Removes this trainer from the weapon after it's been thrown.</summary>
+    /// <remarks>
+    /// Normally this isn't necessary, but something about enemies picking up the thrown weapons
+    /// with this trainer attached causes random(?) <see cref="NullReferenceException"/>s.
+    /// </remarks>
+    public override Boolean HandleEvent(AfterThrownEvent ev) {
+      ev.Item.RemovePart<DeftThrowingTrainer>();
+      return base.HandleEvent(ev);
     }
   }
 
