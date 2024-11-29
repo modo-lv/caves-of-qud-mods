8d4be5cacf4105324d1e17e2b538f28f7b207b48
modo_lv <modo-lv@users.noreply.github.com>
2024-11-30 00:26:31 +0200

    [ItemRecoiler] Better internal names for wishing.

--- ItemRecoiler/Data/IrBlueprintNames.cs
+++ ItemRecoiler/Data/IrBlueprintNames.cs
@@ -6,12 +6,12 @@ namespace ModoMods.ItemRecoiler.Data {
     /// <summary>
     /// The modified <see cref="ProgrammableRecoiler"/> that players activate to recoil items.
     /// </summary>
-    public const String Recoiler = "ModoMods:ItemRecoiler:Recoiler";
+    public const String Recoiler = "ModoMods_ItemRecoiler_Recoiler";
     
     /// <summary>The chest where recoiled items are sent to.</summary>
-    public const String Receiver = "ModoMods:ItemRecoiler:Receiver";
+    public const String Receiver = "ModoMods_ItemRecoiler_Receiver";
     
     /// <summary>The temporary chest that does the actual teleporting.</summary>
-    public const String Transmitter = "ModoMods:ItemRecoiler:Transmitter";
+    public const String Transmitter = "ModoMods_ItemRecoiler_Transmitter";
   }
 }
