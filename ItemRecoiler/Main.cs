efc51a9a9b15bae50151e817fb3a393d24171133
modo_lv <modo-lv@users.noreply.github.com>
2024-11-29 20:31:43 +0200

    [SkillTraining] Startup recoiler gift now correctly adds after entering starting village, not before.

--- ItemRecoiler/Main.cs
+++ ItemRecoiler/Main.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using ModoMods.Core.Utils;
-using ModoMods.ItemRecoiler.Data;
 using ModoMods.ItemRecoiler.Parts;
-using ModoMods.ItemRecoiler.Wiring;
 using XRL;
 using XRL.Wish;
 using XRL.World;
@@ -14,17 +11,8 @@ namespace ModoMods.ItemRecoiler {
       The.Player ?? throw new NullReferenceException("[The.Player] is null.");
 
     public static void Init(GameObject player) {
-      if (ModOptions.GiveOnStartup
-          && !player.Inventory.HasObject(IrBlueprintNames.Recoiler)
-          && !player.HasPart<IrActivationCommand>()) {
-        var recoiler = GameObject.CreateUnmodified(IrBlueprintNames.Recoiler);
-        var text = "You feel a slight spacetime disturbance in your immediate vicinity, " +
-                   $"and quickly discover {recoiler.a}{recoiler.DisplayName} in your inventory " +
-                   "that wasn't there before.";
-        player.Inventory.AddObject(recoiler);
-        Output.Alert(text);
-      }
       player.RequirePart<IrActivationCommand>();
+      player.RequirePart<StartupProvider>();
     }
 
     /// <summary>New game.</summary>
---
+++ ItemRecoiler/Parts/StartupProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using ModoMods.Core.Utils;
+using ModoMods.ItemRecoiler.Data;
+using ModoMods.ItemRecoiler.Wiring;
+using XRL.World;
+
+namespace ModoMods.ItemRecoiler.Parts {
+  /// <summary>
+  /// Provide palyer 
+  /// </summary>
+  [Serializable] public class StartupProvider : ModPart {
+    public Boolean Provided;
+    
+    public override ISet<Int32> WantEventIds => new HashSet<Int32> { BeforeTakeActionEvent.ID };
+
+    public override Boolean HandleEvent(BeforeTakeActionEvent ev) {
+      var player = this.ParentObject;
+      if (ModOptions.GiveOnStartup && !this.Provided) {
+        var recoiler = GameObject.CreateUnmodified(IrBlueprintNames.Recoiler);
+        var text = "You feel a slight spacetime disturbance in your immediate vicinity, " +
+                   $"and quickly discover {recoiler.a}{recoiler.DisplayName} in your inventory " +
+                   "that wasn't there before.";
+        Output.Alert(text);
+        player.Inventory.AddObject(recoiler);
+        this.Provided = true;
+      }
+      return base.HandleEvent(ev);
+    }
+  }
+}
