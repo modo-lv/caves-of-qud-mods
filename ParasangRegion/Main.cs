0520826f0d468586f4ef1b5278ff55e989f39c24
modo_lv <modo-lv@users.noreply.github.com>
2024-12-06 01:08:52 +0200

    [ParasangRegion] Correct composite indicators & don't show on world map.

--- ParasangRegion/Main.cs
+++ ParasangRegion/Main.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Linq;
 using HarmonyLib;
+using XRL;
 using XRL.World;
 
 namespace ModoMods.ParasangRegion {
@@ -8,9 +8,11 @@ namespace ModoMods.ParasangRegion {
   public class Main {
     [HarmonyPostfix][HarmonyPatch(typeof(Zone), nameof(Zone.DisplayName), MethodType.Getter)]
     public static void DisplayName(ref Zone __instance, ref String __result) {
+      if (The.Player?.OnWorldMap() != false)
+        return;
       var x = __instance.X switch { 0 => "W", 1 => "", 2 => "E", _ => "?" };
       var y = __instance.Y switch { 0 => "N", 1 => "", 2 => "S", _ => "?" };
-      var text = x + y;
+      var text = y + x;
       if (text == "") text = "C";
       __result += " ({{C|" + text + "}})";
     }
