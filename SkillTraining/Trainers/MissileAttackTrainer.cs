6968fc4f9eab68c89ff0d74845df9317e22c3fa4
modo_lv <modo-lv@users.noreply.github.com>
2024-12-05 23:54:07 +0200

    [SkillTraining] Split missile attack trainer into trainer and hit detector.

--- SkillTraining/Trainers/MissileAttackTrainer.cs
+++ SkillTraining/Trainers/MissileAttackTrainer.cs
@@ -11,37 +11,38 @@ using XRL.World.Parts;
 namespace ModoMods.SkillTraining.Trainers {
   /// <summary>Trains missile weapon skills.</summary>
   /// <remarks>
-  /// Attached first to the player to listen for missile attack start.
-  /// Once a missile attack is started, also attaches to the intended target,
-  /// to listen for missile hit event.
+  /// Attached to the player to listen for missile attack start, and attach the hit tracker to the target.
   /// </remarks>
   public class MissileAttackTrainer : ModPart {
     public override ISet<Int32> WantEventIds => new HashSet<Int32> { BeforeFireMissileWeaponsEvent.ID };
 
+    /// <summary>Missile weapon attack training.</summary>
+    public override Boolean HandleEvent(BeforeFireMissileWeaponsEvent ev) {
+      if (ev.ApparentTarget.IsCombatant() && ev.Actor.CanTrainSkills())
+        ev.ApparentTarget.RequirePart<MissileHitTracker>();
+      return base.HandleEvent(ev);
+    }
+  }
+
+  /// <summary>Tracks missile hits on targeted enemies.</summary>
+  public class MissileHitTracker : ModPart {
     public override void Register(GameObject gameObject, IEventRegistrar registrar) {
       gameObject.RegisterPartEvent(this, QudEventNames.DefenderProjectileHit);
       base.Register(gameObject, registrar);
     }
 
-    /// <summary>Missile weapon attack training.</summary>
-    public override Boolean HandleEvent(BeforeFireMissileWeaponsEvent ev) {
-      if (ev.ApparentTarget.IsCombatant() && ev.Actor.CanTrainSkills())
-        ev.ApparentTarget.RequirePart<MissileAttackTrainer>();
-      return base.HandleEvent(ev);
-    }
-
     public override Boolean FireEvent(Event ev) {
       if (ev.ID != QudEventNames.DefenderProjectileHit)
         return base.FireEvent(ev);
-      
+
       var launcher = ev.GetGameObjectParameter("Launcher");
       var skillClass = ev.GetStringParameter("Skill");
       var attacker = ev.Attacker()?.OnlyIf(it => it.CanTrainSkills());
       var defender = ev.Defender()?.OnlyIf(it => it.IsCombatant());
-      
+
       if (launcher == null || attacker == null || defender == null || skillClass == null)
         return base.FireEvent(ev);
-      
+
       var action = skillClass switch {
         // AFAICT there are no weapons using "Bow" skill anymore, but the game has checks for it.
         "Bow" => PlayerAction.BowOrRifleHit,
@@ -57,7 +58,7 @@ namespace ModoMods.SkillTraining.Trainers {
          ) {
         attacker.TrainingTracker()?.HandleTrainingAction(action);
       }
-      
+
       return base.FireEvent(ev);
     }
   }
